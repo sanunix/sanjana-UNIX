ec867d3964f0ccb3d90e1e0dfb6d85ff6beb8efc
sanunix <35561882+sanunix@users.noreply.github.com>
2018-02-22 09:01:54 +0530

    Add files via upload

---
+++ sanstringop.sh
@@ -0,0 +1,39 @@
+echo "string operations"
+echo "enter the first string to check "
+read a
+echo "Enter the second string to check for operations"
+read b
+if [ $a = $b ]
+then
+	   echo "$a = $b : $a is equal to $b"
+   else
+	      echo "$a = $b: $a is not equal to $b"
+      fi
+
+      if [ $a != $b ]
+      then
+	         echo "$a != $b : $a is not equal to $b"
+	 else
+		    echo "$a != $b: $a is equal to $b"
+	    fi
+
+	    if [ -z $a ]
+	    then
+		       echo "-z $a : string length is zero"
+	       else
+		          echo "-z $a : string length is not zero"
+		  fi
+
+		  if [ -n $a ]
+		  then
+			     echo "-n $a : string length is not zero"
+		     else
+			        echo "-n $a : string length is zero"
+			fi
+
+			if [ $a ]
+			then
+				   echo "$a : string is not empty"
+			   else
+				      echo "$a : string is empty"
+			      fi
